0d7d49ac8ab7ce881eddf35b3d079c966290ae80
TonyCross23 <tonychamp40@gmail.com>
2025-03-14 15:42:18 +0630

    product

--- microservices/customer/src/database/connection.js
+++ microservices/customer/src/database/connection.js
@@ -4,10 +4,7 @@ const { DB_URL } = require('../config');
 module.exports = async() => {
 
     try {
-        await mongoose.connect(DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(DB_URL);
         console.log('Db Connected');
         
     } catch (error) {
